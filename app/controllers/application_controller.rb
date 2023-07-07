75019f3cec0db7a928658ab39b2da7209effb5c8
gacurl <greg.a.curl@gmail.com>
2023-07-07 14:26:03 -0400

    update homepage, add footer, complete styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  private
+  def authenticate_user!
+    if user_signed_in?
+      root_path
+    else
+      render 'projects/home', notice: "Please Login to view that page!"
+    end
+  end
 end
